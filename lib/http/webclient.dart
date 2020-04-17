936f1d01f1b9496ba410742997635f0b10554889
fabiovalinhos <fabiovalinhos@gmail.com>
2020-04-17 07:50:59 -0300

    Salvando transferências

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -25,12 +25,15 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+const String baseUrl = 'http://192.168.0.14:8080/transactions';
+
+final Client client = HttpClientWithInterceptor.build(interceptors: [
+  LoggingInterceptor(),
+]);
+
 Future<List<Transaction>> findAll() async {
-  final Client client = HttpClientWithInterceptor.build(interceptors: [
-    LoggingInterceptor(),
-  ]);
   final Response response =
-      await client.get('http://192.168.0.14:8080/transactions').timeout(Duration(seconds: 5));
+      await client.get(baseUrl).timeout(Duration(seconds: 5));
 
   // convertendo o json, que é uma string, para um objeto
   final List<dynamic> decodedJson = jsonDecode(response.body);
@@ -48,4 +51,32 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async{
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber,
+    }
+  };
+
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(baseUrl, headers: {
+    'Content-type': 'application/json',
+    'password': '1000',
+  }, body: transactionJson,);
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+    return Transaction(
+      json['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
 }
